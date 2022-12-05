60bab89d716aa38a34dbd9ab9583351788856f84
Jonathan Gray <zeusdcs@gmail.com>
2022-12-05 03:08:28 -0400

    re-added / route

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const
 
 app.use(express.static(path.join(__dirname, "build")))
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "build", "index.html"))
-// })
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
-// app.get("/23", (req, res) => {
-//     res.sendFile(path.join(__dirname, "build/static/media/23_new.4054b34b9b168988d2b3.png"))
-// })
+app.get("/23", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/static/media/23_new.4054b34b9b168988d2b3.png"))
+})
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"))
